2dd3c7f3668273f5fc7f74ee20d75201ca8f970b
Igor Gonçalves <igordossantos@gmail.com>
2021-08-31 15:18:35 -0300

    It was the teacher solution, using the same structure with a little difference

--- ex02.js
+++ ex02.js
@@ -21,3 +21,19 @@ console.log(typeTriagle(2, 2, 2))
 console.log(typeTriagle(6, 2, 2))
 console.log(typeTriagle(2, 3, 5))
 
+
+// Teacher solution ---> He has used the same structura but in the last comparation decided don't explicit the business rule
+
+function classificaTriangulo (lado1, lado2, lado3) {
+    if(lado1 == lado2 && lado2 == lado3){
+        return 'Equilátero'
+    } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        return 'Isósceles'        
+    }else { 
+        return 'Escaleno'
+    }
+}
+
+console.log(classificaTriangulo(2, 2, 2));
+console.log(classificaTriangulo(2, 3, 3));
+console.log(classificaTriangulo(2, 3, 4));
