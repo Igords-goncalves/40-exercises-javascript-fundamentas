943db37f290193286f71ea15dab6da430c6fd45f
Igor Gonçalves <igordossantos@gmail.com>
2021-08-31 14:37:48 -0300

    It was the teacher solution, using a concept more basic of Params Variables

--- ex01.js
+++ ex01.js
@@ -1,14 +1,20 @@
-//Using a method Function Factory
+//My solution --> Using a method Function Factory
 
-function calcular(a, b) {
+function calculate(a, b) {
    return {
-        soma: a + b,
-        sub: a - b,
-        mult: a * b,
-        div: a / b
+        sum: a + b,
+        subtract: a - b,
+        multiply: a * b,
+        divided: a / b
    }
 }
+console.log(calculate(2, 4))
+console.log(calculate(3, 9))
 
-console.log(calcular(2, 4))
-console.log(calcular(3, 9))
+//Teacher Solution --> Using Params Variables 
 
+function calcularOperacoes (operador1, operador2) {
+    console.log(operador1+operador2, operador1-operador2, operador1*operador2, operador1/operador2);
+}
+
+calcularOperacoes(1, 2)
