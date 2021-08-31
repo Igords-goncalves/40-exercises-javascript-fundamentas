1e1c4f028fd4ae185f787b9bb424530e5cf75d58
Igor Gonçalves <igordossantos@gmail.com>
2021-08-31 15:35:27 -0300

    My way using simple things

---
+++ ex04.js
@@ -0,0 +1,7 @@
+//May solution --> Using a abstract arro function and a concatenation
+const moduleDivisor = (x, y) => x / y + " with rest of divison " + x % y
+
+console.log(moduleDivisor(10, 6))
+console.log(moduleDivisor(4, 2))
+
+//Teacher Solutin -->
