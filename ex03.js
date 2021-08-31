bb7933416b7ad1e1901fa5631d682cc669a3ed96
Igor Gonçalves <igordossantos@gmail.com>
2021-08-31 15:24:14 -0300

    Teacher's way, using the samething

--- ex03.js
+++ ex03.js
@@ -5,3 +5,16 @@ const exponent = (x, y) => {
 
 console.log(exponent(2, 2))
 
+
+// Teacher solution ---> The same thing
+function expoente (base, expoente) {
+    //Podemos fazer de 2 modos
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(expoente(2, 3))
