a58500f688192e5987c7eac543cc4a657d539d12
Igor Gonçalves <igordossantos@gmail.com>
2021-08-31 15:45:23 -0300

    Right form using 3 methods

--- ex05.js
+++ ex05.js
@@ -6,3 +6,10 @@ function simpleNumber (a = 0.30000000000000004) {
 
 console.log(simpleNumber())
 
+//Teacher way --> I don't pay attention to the question 
+function formatarValorDecimal(valorDecimal) {
+    valorEmReais = `R$ ${valorDecimal.toFixed(2).toString().replace(".", ",")}`
+    console.log(valorEmReais)
+}
+
+formatarValorDecimal(0.1 + 0.2)
