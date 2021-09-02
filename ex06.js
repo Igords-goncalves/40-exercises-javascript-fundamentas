1d62fa1d9eb4c327d303be1f356e1bfa0c14c28d
Igor Gonçalves <igordossantos@gmail.com>
2021-09-02 10:48:43 -0300

    compundInterest is retunr a string not a number it's a problem in my program

--- ex06.js
+++ ex06.js
@@ -6,16 +6,17 @@
 //M = C(1 + i)^t --> Juros Compostos
 
 function simpleInterest(C, i, t) {
-    let calc = C * i * t
-    let compoundInterest = function() {
-        let result = C * ((1 + i) ** t)
+    let calc = C * i * t //Escopo Local
+
+    let compoundInterest = () => {
+        let result = C * ((1 + i) ** t) //Escopo Local
         return result
     }
     console.log(C + calc)
     return compoundInterest().toFixed(2)
 }
 
-const amount = simpleInterest(100, 0.10, 2)
+const amount = simpleInterest(1000, 0.10, 2)
 console.log('$'+ amount)
 
 
