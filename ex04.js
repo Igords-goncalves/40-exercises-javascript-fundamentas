67cf44a7b073a14f6bbcbfed852a5e0c1e404e0f
Igor Gonçalves <igordossantos@gmail.com>
2021-09-02 14:24:34 -0300

    Update in my function

--- ex04.js
+++ ex04.js
@@ -1,5 +1,5 @@
 //May solution --> Using a abstract arro function and a concatenation
-const moduleDivisor = (x, y) => x / y + " with rest of divison " + x % y
+const moduleDivisor = (x, y) => Math.floor(x / y) + " with rest of divison " + x % y
 
 console.log(moduleDivisor(10, 6))
 console.log(moduleDivisor(4, 2))
