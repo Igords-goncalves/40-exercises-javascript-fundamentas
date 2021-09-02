bd02da4c78ba78000aad6cc3ea66ce6001e40861
Igor Gonçalves <igordossantos@gmail.com>
2021-09-02 08:57:38 -0300

    Making the first part of program

---
+++ ex06.js
@@ -0,0 +1,22 @@
+//My way -->
+
+// J = C * i * t --> Juros Simples
+// M = C + J --> Montante
+
+//M = C(1 + i)^t --> Juros Compostos
+
+function jurosSimples(C, i, t) {
+    return C * i * t
+}
+
+function jurosCompostos(jurosSimples) {
+    
+}
+
+
+let result = jurosSimples(1000, 0.10, 2)
+console.log(result);
+
+
+
+//Teacher way -->
