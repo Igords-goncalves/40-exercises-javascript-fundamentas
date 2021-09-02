c3f6f5ff3c9ddc2b380e46ebb38ef246800d759c
Igor Gonçalves <igordossantos@gmail.com>
2021-09-02 12:10:01 -0300

    Solve question with dificult

--- ex07.js
+++ ex07.js
@@ -4,25 +4,28 @@
 // x = - b +- √Δ / 2 * a
 // Return x' and x"
 
-//My way --> 
+//My way --> The problem in my was, a little of math, not use the push method and not see that result can would be a array 
 
-//Equation: 3x² - 5x + 12 = 0
+//Equation standard: 3x² - 5x + 12 = 0
 
 const baskhara = (a = 3 , b = - 5, c = 12) => { //I can allready use standard params
-    let result = []
-    let delta = (b ** 2)  - (4 * (a * c))
+    let result = [] //Looking teacher
+    let delta = (b ** 2)  - (4 * (a * c)) 
 
     if (delta < 0) {
         console.log('Negative Delta')
     } else { 
-        let x1 = -(-5 + Math.sqrt(delta) / 2 * a);
-        let x2 = -(-5 - Math.sqrt(delta) / 2 * a);
-        result.push(x1)
-        result.push(x2)
-        return result //O método é o push
+        console.log('Delta is: '+ delta)
+        // let x =  -(-b + Math.sqrt(delta)) / 2 * a --> x' and x"
+
+        let x1 = -(-b + Math.sqrt(delta)) / 2 * a; //Looking teacher
+        let x2 = -(-b - Math.sqrt(delta)) / 2 * a; //Looking teacher
+        result.push(x1) //Looking teacher
+        result.push(x2) //Looking teacher
+        return result //Looking teacher
     }
 }
 
-console.log(baskhara(1, 3, 2)) //Traying to oder things
+console.log(baskhara(1, -1, -12)) //Traying to oder things
 
 //Teacher way -->
