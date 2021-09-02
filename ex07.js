cea8d520b78038cd6d80e8e0bb40274c0d685bd0
Igor Gonçalves <igordossantos@gmail.com>
2021-09-02 12:11:06 -0300

    Teacher using simples things again

--- ex07.js
+++ ex07.js
@@ -28,4 +28,20 @@ const baskhara = (a = 3 , b = - 5, c = 12) => { //I can allready use standard pa
 
 console.log(baskhara(1, -1, -12)) //Traying to oder things
 
-//Teacher way -->
+//Teacher way --> Teacher is using more simple way
+
+function bhaskara (ax2, bx, c) {
+    let resultados = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return "Delta é negativo"
+    } 
+    let x1 = (-bx + Math.sqrt(delta))/2*ax2
+    let x2 = (-bx - Math.sqrt(delta))/2*ax2
+    resultados.push(x1)
+    resultados.push(x2)
+    return resultados   
+}
+
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
