4f265fe83d0b9ed6cdd839f7df9e550c7dffe92d
Igor Gonçalves <igordossantos@gmail.com>
2021-09-08 10:00:16 -0300

    Exercise bad fomulated by course

--- ex09.js
+++ ex09.js
@@ -1,13 +1,14 @@
-// Todo aluno recebe uma nota de 0 a 100
-// Alunos com nota abaixo de 40 são reprovados.
+/* As notas possuem as seguintes regras de arredondamento (Regras de Negócio)
 
-/* As notas possuem a seguinte regra de arredondamento (Regras de Negócio)
+--> Todo aluno recebe uma nota de 0 a 100
 
---> Se a diferença entre a nota e o próximo múltiplo de 5 for menor que 3, 
-arredondar a nota para esse próximo múltiplo de 5 --> Quem são os múltiplos de 5?
+--> Alunos com nota abaixo de 40 são reprovados (Regra Falsa)
 
 --> Se a nota for ABAIXO grade < 38 ---> de 38, não é feito nenhum arredondamento pois esta nota 
 resulta na reprovação do aluno
+
+--> Se a diferença entre a nota e o próximo múltiplo de 5 for menor que 3, 
+arredondar a nota para esse próximo múltiplo de 5 --> Quem são os múltiplos de 5?
 
 --> Por exemplo, a nota 84 será arredondada para 85, 
 mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, 
@@ -32,15 +33,20 @@ function gradesSystem(firstTwoMonths = 0, secondTwomonths = 0, thirdTwoMonths = 
 
     const finalGrade = (ftm + stm + ttm + fth) / 4
 
-    let roundingGrades = (grade) => {
-        if(grade < 40) {
+    let roundingGradesRules = (grade) => { // grade === finalGrade 
+        if(grade < 38) {
             console.log('DISAPPROVED')
+        } else if (grade > 38) { //Rounding to up
+            if (grade % 5 < 3) {
+            }
+            let rounded = Math.ceil(grade + (grade % 5))
+            console.log('APROVED')
+            console.log("Grade rounded = " + rounded) //Teste
         }
     }
-    roundingGrades(finalGrade)
+
+    console.log(`Your final grade is ${finalGrade}`)
+    roundingGradesRules(finalGrade)
 }
 
-
-
-
-gradesSystem(100, 10, 20); //Array grades or number ?
+gradesSystem(91.34, 51, 0); //Array grades or number ?
