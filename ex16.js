780ade9b71611d86c79f0fc1d04628515c45a624
Igor Gonçalves <igordossantos@gmail.com>
2021-09-15 15:33:38 -0300

    Simple solutions

--- ex16.js
+++ ex16.js
@@ -1,18 +1,16 @@
-function calculator(number1, number2) {
-    let calc = (operator) => {
-        switch (operator) {
-            case "+":
-                return number1 + number2
-            case "-":
-                return number1 - number2
-            case "*":
-                return number1 - number2
-            case "/":
-                return number1 - number2
-            default:
-                return number1 + number2
-        }
+function calculator(number1, operator, number2) {
+    switch (operator) {
+        case "+":
+            return number1 + number2
+        case "-":
+            return number1 - number2
+        case "*":
+            return number1 - number2
+        case "/":
+            return number1 - number2
+        default:
+            return number1 + number2
     }
 }
 
-console.log(calculator(2, 4))
+console.log(calculator(2, "+", 4))
