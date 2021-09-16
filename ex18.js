f669849a39ea96615147ed8437c000250710f1bb
Igor Gonçalves <igordossantos@gmail.com>
2021-09-16 09:22:57 -0300

    Simple is better

---
+++ ex18.js
@@ -0,0 +1,12 @@
+function counter(number) {
+    switch (number) {
+        case 0:
+            console.log('zero')
+            break;
+        case 1:
+            console.log('one')
+        default:
+            console.log('It´s not a number')
+            break;
+    }
+}
