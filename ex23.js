4dc0f1f254434d993a23b007beced4727aa140d7
Igor Gonçalves <igordossantos@gmail.com>
2021-09-24 18:52:29 -0300

    updates

---
+++ ex23.js
@@ -0,0 +1,13 @@
+//Algoritmo de boletim escolar
+
+//O aluno tem 3 notas
+
+//Calcule a média ponderada do aluno considerando que o peso para a maior nota seja 4 e para as duas restantes, 3.
+
+//"APROVADO" se a média for maior ou igual a 5 e
+
+//"REPROVADO" se a média for menor que 5.
+
+//Repita a operação até que o código lido seja negativo.
+
+
