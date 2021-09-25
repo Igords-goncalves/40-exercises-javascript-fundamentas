a4a6d8518ebeb8fe878cd5bc50c20097ccd0f65e
Igor Gonçalves <igordossantos@gmail.com>
2021-09-25 17:51:53 -0300

    Simple is better

---
+++ ex26.js
@@ -0,0 +1,11 @@
+// Todos os numeros pares entre 1 e 100
+
+function even(number) {
+    for (let i = 1; i < number; i++) {
+        if (i % 2 == 0) {
+            console.log(`Even ${i}`);
+        }
+    }
+    console.log(total)
+}
+even(100)
