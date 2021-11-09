7d7374d354bb6dda33860d26319c6a7ccb4bab7b
Igor Gonçalves <igordossantos@gmail.com>
2021-11-09 08:31:58 -0300

    Evoluindo na regra de negocio, porém é necessário enxugar as notas e entregar a melhor distribuição possivel, deixar a let contagem menos adaptata

--- ex20.js
+++ ex20.js
@@ -12,6 +12,7 @@
 function valorDoUsuario(valor) {
    const notas = [100, 50, 10, 5, 1]
    let cedulas = 0 //Recebe 0 agora pois vai receber o numero de cedulas depois
+   let contagem = ''
 
 
    for(let i = 0; i < notas.length; i++) {
@@ -21,10 +22,14 @@ function valorDoUsuario(valor) {
 
             // Especificando a quantidade de cada nota(elemento) dentro do array
             cedulas = (valor - (valor % notas[i])) / notas[i]
-            return `${cedulas} de R$${notas[i]}` 
+
             //
+
+
+            // contagem das notas e impressão
+            contagem = contagem + `${cedulas} de R$${notas[i]} `
         }
     }
-    return cedulas
+    return contagem
 }
-console.log(valorDoUsuario(380))
+console.log(valorDoUsuario(400))
