268a0584bf6e5811bc37573ebd0b9f198af0a075
Igor Gonçalves <igordossantos@gmail.com>
2021-11-09 08:09:25 -0300

    Evoluindo na regra de negocio

--- ex20.js
+++ ex20.js
@@ -3,21 +3,28 @@
 
  !O objetivo é entregar o minimo de cédulas, ou seja, otimizar o troco.
 
- Por exemplo, ao informar 18, o programa deve
- informar apenas a seguinte informação (note que não foram exibidas 
- informações sobre as demais cédulas):
- 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1. 
+ * Por exemplo, ao informar 18, o programa deve
+ * informar apenas a seguinte informação (note que não foram exibidas 
+ * informações sobre as demais cédulas):
+ * 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1. 
 */
 
 function valorDoUsuario(valor) {
    const notas = [100, 50, 10, 5, 1]
+   let cedulas = 0 //Recebe 0 agora pois vai receber o numero de cedulas depois
+
 
    for(let i = 0; i < notas.length; i++) {
-        if (valor > notas[i]) {
-            const troco = valor / notas[i]
-            console.log(`${troco} notas de ${notas[i]}`)
+        if (valor >= notas[i]) {
+            
+            //Regra de negócio
+
+            // Especificando a quantidade de cada nota(elemento) dentro do array
+            cedulas = (valor - (valor % notas[i])) / notas[i]
+            return `${cedulas} de R$${notas[i]}` 
+            //
         }
     }
+    return cedulas
 }
-
-valorDoUsuario(500)
+console.log(valorDoUsuario(380))
