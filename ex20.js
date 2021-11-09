dc43306a72cb395742269887788b95e43ece305a
Igor Gonçalves <igordossantos@gmail.com>
2021-11-09 09:07:24 -0300

    Adicionando a funcionalidade de plural ou singular na impressão do console

--- ex20.js
+++ ex20.js
@@ -12,8 +12,8 @@
 function valorDoUsuario(valor) {
    const notas = [100, 50, 10, 5, 1]
    let cedulas = 0 //Recebe 0 agora pois vai receber o numero de cedulas depois
+   let valorSubtraido
    let contagem = ''
-
 
    for(let i = 0; i < notas.length; i++) {
         if (valor >= notas[i]) {
@@ -23,13 +23,19 @@ function valorDoUsuario(valor) {
             // Especificando a quantidade de cada nota(elemento) dentro do array
             cedulas = (valor - (valor % notas[i])) / notas[i]
 
-            //
-
+            //! Distribuição das notas visando o minimo de notas para atingir o valor
+            valorSubtraido = valor - (valor % notas[i])
+            valor -= valorSubtraido
 
             // contagem das notas e impressão
-            contagem = contagem + `${cedulas} de R$${notas[i]} `
+            if (cedulas > 1) {
+                contagem = contagem + `${cedulas} notas de R$${notas[i]},00 `
+            } else {
+                contagem = contagem + `${cedulas} nota de R$${notas[i]},00 `
+            }
         }
+
     }
     return contagem
 }
-console.log(valorDoUsuario(400))
+console.log(valorDoUsuario(350))
