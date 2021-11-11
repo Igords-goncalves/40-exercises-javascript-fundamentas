922baa46cddc2401627b10b6383f18fad0c1201f
Igor Gonçalves <igordossantos@gmail.com>
2021-11-11 11:06:11 -0300

    Exercicio finalizado, trabalhando nas melhorias do código

--- ex27.js
+++ ex27.js
@@ -2,7 +2,7 @@
 
 //// existe uma criança menor?
 
-// Em quantos anos a crinaça menor vai ultrapassar a maior?
+//// Em quantos anos a crinaça menor vai ultrapassar a maior?
 
 //// Unidade padrão centimetros
 
@@ -11,33 +11,53 @@ function crescimentoKids(altura1, taxa1, altura2, taxa2) {
     let menorAltura
 
     if (altura1 < altura2) {
-        menorAltura = `A criança com menor altura tem ${altura1}`
+        console.log('\n-----------------------------------------')
+        menorAltura = `A criança 1 tem menor altura ${altura1}cm`
     } else if (altura1 > altura2) {
-            menorAltura = `A criança com menor altura tem ${altura2}`
+            console.log('\n-----------------------------------------')
+            menorAltura = `A criança 2 tem menor altura ${altura2}cm`
         } else {
+            console.log('\n-----------------------------------------')
             menorAltura = 'As alturas das crianças são iguais'
         }
-        console.log(menorAltura)
+        console.log(menorAltura + '\n')
 
-    let txCrescimento1 = altura1 * taxa1
-    let txCrescimento2 = altura2 * taxa2
-
-    console.log(`A criança com altura ${altura1} tem a taxa de crescimento ${txCrescimento1}`)
-    console.log(`A criança com altura ${altura2} tem a taxa de crescimento ${txCrescimento2}`)
+    const txCrescimento1 = altura1 * (taxa1 / 100)
+    const txCrescimento2 = altura2 * (taxa2 / 100)
 
     let crescimentoAnual1 = altura1 + txCrescimento1
     let crescimentoAnual2 = altura2 + txCrescimento2
 
-    console.log(crescimentoAnual1)
-    console.log(crescimentoAnual2)
-    
     let ano = 0
+    
+    //Verifcação 1
+    if (crescimentoAnual1 < crescimentoAnual2) {
+        while (crescimentoAnual1 < crescimentoAnual2) {
+            ano++
+            crescimentoAnual1 += txCrescimento1
+        }
 
-    while (crescimentoAnual1 < crescimentoAnual2) {
-        ano++
-        crescimentoAnual1 += txCrescimento1
-        console.log(ano)
-        console.log(crescimentoAnual1)
+        if (ano > 12) {
+            console.log('A criança 1 não ultrapassará a criança 2 na fase infantil\n')
+        } else {
+            console.log(`Em ${ano} anos a criança 1 será maior que a criança com altura 2 \n`)
+        }
+        // Verificação 2
+    } else if (crescimentoAnual2 < crescimentoAnual1) {
+        while (crescimentoAnual2 < crescimentoAnual1) {
+            ano++
+            crescimentoAnual2 += txCrescimento2
+        }
+        
+        if (ano > 12) {
+            console.log('A criança 2 não ultrapassará a criança 1 na fase infantil \n')
+        } else {
+            console.log(`Em ${ano} anos a criança 2 será maior que a criança com altura 1 \n`)
+        }
     }
 }
-crescimentoKids(130, 0.3, 140, 0.4)
+crescimentoKids(110, 2, 140, 10)
+crescimentoKids(140, 10, 140, 2)
+crescimentoKids(160, 4, 160, 2) //Adolescentes
+crescimentoKids(60, 2, 40, 6) //Bebês
+crescimentoKids(40, 1, 40, 5)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+crescimentoKids(140, 10, 120, 2)
+// crescimentoKids(160, 4, 160, 2) //Adolescentes
+// crescimentoKids(110, 4, 140, 2)
+// crescimentoKids(60, 2, 40, 6) //Bebês
