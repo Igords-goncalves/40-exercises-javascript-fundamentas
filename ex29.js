6705317381e398c5d31661b621c1b35b7c4b6d31
Igor Gonçalves <igordossantos@gmail.com>
2021-11-25 11:07:38 -0300

    Exercicio finalizado

--- ex29.js
+++ ex29.js
@@ -1,7 +1,20 @@
-// Utilize o for
+//// Utilize o for
 
-// A função deve percorrer o vetor
+//// A função deve percorrer o vetor
 
 //? Quantos numeros estão no intervalo [10,20] incluindo o 10 e o 20?
 
 //? Quantos deles estão fora do intervalo?
+
+function percorrerVetor(valor) {
+    let totalDeNumeros = 0
+    for (let index = 0; index < valor.length; index++) {
+        if (valor[index] >= 10 && valor[index] <=20) {
+            totalDeNumeros ++
+        }
+    }
+    console.log(`O total de numeros entre 10 e 20 é: ${totalDeNumeros}`);
+}
+
+percorrerVetor([17,1,2,3,4,5,6,7,8,9,10,2,0,15,16,20]);
+percorrerVetor([4,5,6,7,8,9,10,2,16,20]);
