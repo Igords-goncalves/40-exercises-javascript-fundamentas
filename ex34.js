0be814ca7add5ba8a11d77eb4a8fc73edf1e43a1
Igor Gonçalves <igordossantos@gmail.com>
2021-11-25 15:19:10 -0300

    Verificação Funcionando

---
+++ ex34.js
@@ -0,0 +1,24 @@
+//// Função vai receber 2 strings como parametro
+
+// Retornar true or false caso os caracteres maiusculos ou minuscolos estejam na mesma palavra
+
+function verificarCaracteres(string1, string2) {
+    for (let i = 0; i < string1.length; i++) {
+        let verify1 = string1.charAt(i).toUpperCase()
+
+        for (let j = 0; j < string2.length; j++) {
+            let verify2 = string2.charAt(j).toUpperCase()
+
+            if ( verify1 == verify2) {
+                return true // Tem os mesmos caracteres
+            } else { 
+                return false // Tem caracteres diferentes
+            }
+        }
+    }
+}
+
+console.log(verificarCaracteres("Armando", "armando"))
+console.log(verificarCaracteres("Armando", "bananada"))
+console.log(verificarCaracteres("Armando", "r"))
+
