65da6a31f019773a9e44046a9ec5922b9b183cb0
Igor Gonçalves <igordossantos@gmail.com>
2021-12-02 17:31:57 -0300

    Feito olhando a solução do professor

--- ex38.js
+++ ex38.js
@@ -4,14 +4,20 @@
 
 // Se ao passar os parametros a ordem for inversa corrija isso
 
-function contadorImpar(min = 0, max = 100) {
-    while(min < max) {
-        if(min > max) {
-            let maior = (max + min) - max // (100 + 200) - 100 = 200
-            let menor = (min + max) - min // (200 + 10) - 200 = 150
-        }
+function contadorImpar(inicio = 0, fim = 100) {
+    if (inicio > fim) {
         
+        // Lógica para troca de valores
+        inicio = fim + inicio //  2 + 17 = 19
+        fim = inicio - fim // 19 - 2 = 17 fim
+        inicio = inicio - fim // 19 - 17 = 2 incio
+    }
+
+    for (let cont = inicio; cont < fim; cont++) {
+        if (cont % 2 == 1) {
+            console.log(cont)
+        }
     }
 }
 
-contadorImpar(320, 80)
+contadorImpar(17, 2)
